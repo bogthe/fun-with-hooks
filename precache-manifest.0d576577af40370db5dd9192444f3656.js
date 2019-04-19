57a30560782f425b8d7a1d62b2f13d0fbf1680d9
Bogdan Stolojan <stolojan.bogdan@gmail.com>
2019-04-19 12:36:10 +0100

    Updates

---
+++ precache-manifest.0d576577af40370db5dd9192444f3656.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "caad70552c39825b3400",
+    "url": "/fun-with-hooks/static/js/runtime~main.6ff143c1.js"
+  },
+  {
+    "revision": "250410b7ed561a6bbefc",
+    "url": "/fun-with-hooks/static/js/main.b130284e.chunk.js"
+  },
+  {
+    "revision": "2faef6bc8214d1135160",
+    "url": "/fun-with-hooks/static/js/2.f09c5d3d.chunk.js"
+  },
+  {
+    "revision": "250410b7ed561a6bbefc",
+    "url": "/fun-with-hooks/static/css/main.63f25f35.chunk.css"
+  },
+  {
+    "revision": "e30b8fbe2eb3c5b9309adcd2ce25d7d7",
+    "url": "/fun-with-hooks/index.html"
+  }
+];
